c62690a5d8afff7c7aee3f45c45d7fdcf760d4da
Jonathan Manera <jonathan.manera@globallogic.com>
2020-06-04 23:00:30 -0300

    js

---
+++ 06.Javascript/02.control-flow/script.js
@@ -0,0 +1,131 @@
+// if...else
+var a = 2,
+    b = 2;
+
+if (a == b) {
+    console.log("a eq b")
+}
+
+if (a == 2 && b == 2) {
+    console.log("a and b = 2")
+}
+
+if (a == 1 || a == 2 || a == 3) {
+    console.log("a is 1 or 2 or 3")
+}
+
+a++
+if (a == b) {
+    console.log("a eq b")
+} else {
+    console.log("a not eq b")
+}
+
+a++
+if (a == 1) {
+    console.log("a is 1")
+} else if (a == 2) {
+    console.log("a i2 2")
+} else if (a == 3) {
+    console.log("a is 3")
+} else {
+    console.log("a is not 1, 2 or 3")
+}
+
+// switch...case
+switch (a) {
+    case 1:
+        console.log("a is 1");
+        break;
+    case 2:
+        console.log("a is 2");
+        break;
+    case 3:
+        console.log("a is 3");
+        break;
+    case 4:
+        console.log("a is 4");
+        break;
+    default:
+        console.log("a is not 1, 2, 3 or 4")
+}
+
+// for
+for (var i = 0; i <= 10; i++) {
+    console.log("i is " + i);
+}
+
+// while
+var x = 50;
+while (x != 0) {
+    if (x % 5 == 0) {
+        console.log(x + " is divisible by 5");
+    }
+    x--
+}
+
+// do...while
+var isTrue = true;
+y = 100;
+do {
+    if (y == 10) {
+        console.log("y is 10");
+        isTrue = false;
+    }
+    y--
+
+} while (isTrue);
+
+// Arrays
+var drinks = ['Wine', 'Beer', 'Coffe', 'Soda'];
+console.log("drinks have " + drinks.length + " items");
+console.log(drinks);
+
+console.log(drinks[0]);
+console.log(drinks[1]);
+console.log(drinks[2]);
+console.log(drinks[3]);
+
+// add item
+drinks[4] = 'Tea';
+console.log(drinks[4]);
+
+// loop an array
+for (var drink of drinks) {
+    console.log('I like a ' + drink)
+}
+
+// functions with arrays
+var names = new Array('John', 'Bob', 'Zoe');
+console.log(names);
+
+// add last
+names.push('Pedro');
+console.log(names);
+
+// add first
+names.unshift('Lili');
+console.log(names);
+
+// remove last
+names.pop();
+console.log(names);
+
+// remove first
+names.shift();
+console.log(names);
+
+// remove items from x to y
+names.splice(1, 2);
+console.log(names);
+
+// get index
+var pos = names.indexOf('Carlos');
+console.log(pos);
+
+// remove item
+var index = names.indexOf('John')
+if (index != -1) {
+    names.splice(index, 1);
+}
+console.log(names);
---
+++ 06.Javascript/03.functions/script.js
@@ -0,0 +1,43 @@
+// Consumer
+function print(nombre) {
+    console.log('Hello ' + nombre);
+}
+
+print('Anna');
+
+// Supplier
+function getMessage() {
+    return "Javascript is cool";
+}
+
+console.log(getMessage());
+
+// Func
+function f(input1, input2) {
+    if (typeof(input1) === 'number' && typeof(input2) === 'number') {
+        return +input1 + input2;
+    } else {
+        return input1 + " " + input2;
+    }
+
+}
+
+console.log(f(2,2));
+console.log(f("Dos",2));
+console.log(f("Dos","Dos"));
+
+// Recursive func
+function factorial(num) {
+    if(num==0) {
+        return 1;
+    }
+    return num*factorial(num-1);
+}
+
+console.log("the factorial of 10 is "+ factorial(10));
+
+// Annonymous func
+var sum = (a,b) => a + b;
+console.log("sum is "+sum(10, 12));
+
+
